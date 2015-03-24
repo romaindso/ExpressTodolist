28b28e20b34e564e86da269dbe29c22968dca13f
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-03-24 20:51:02 +0100

    Add delete icon

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,9 @@ $(function() {
 
 	function appendTasksList(tasks){
 		for(var i=0; i<tasks.length;i++){
-			$('.tasks-list').append('<li>'+tasks[i]+'</li>');
+			$('.tasks-list').append(
+				'<li>'+tasks[i]+' <a href="#" data-title="'+tasks[i]+'"><i class="fa fa-trash-o"></i></a></li>'
+			);
 		}		
 	}
 
@@ -22,5 +24,12 @@ $(function() {
 				console.log(err);
 			}
 		});
+	});
+
+	$('.tasks-list').on('click', 'a[data-title]', function(event){
+		if(!(confirm('Are you sure ?'))){
+			return false;
+		}
+		console.log('go delete');
 	});
 });
