2b3f5a3ee5bc24e10070973d5fd58d907e390e0e
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-04-15 17:57:30 +0200

    Replace Redis by MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
+
+mongoose.connect('mongodb://localhost/todolist');
 
 app.use(express.static(__dirname + '/public'));
 app.use('/tasks', require('./routes/tasks'));
